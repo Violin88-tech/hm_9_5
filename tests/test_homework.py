23d564faf35e779a53ee21f87153e7d2f9eb188d
Viktoria <48157182+violin88@users.noreply.github.com>
2024-01-20 17:55:58 +0400

    homework 1

---
+++ tests/test_homework.py
@@ -0,0 +1,33 @@
+from demoqa_tests.pages.registration_pages import RegistrationPage
+
+
+def test_practice_from():
+    registration_page = RegistrationPage()
+    registration_page.open_registration_page()
+
+    registration_page.fill_first_name('Vika')
+    registration_page.fill_last_name('Islenteva')
+    registration_page.fill_user_email('email@gmail.com')
+    registration_page.gender_selection('Female')
+    registration_page.fill_user_phone_number(9920454526)
+    registration_page.fill_date_of_birth('2003', 'June', '03')
+    registration_page.select_user_subject('Economics')
+    registration_page.user_hobby_checkbox('Sports')
+    registration_page.user_picture('photo.jpg')
+    registration_page.user_current_adress('Artistic driveway 4, sq.1')
+    registration_page.user_state('NCR')
+    registration_page.user_city('Noida')
+    registration_page.submit_the_form()
+
+    registration_page.should_registered_user_with(
+        'Vika Islenteva',
+        'email@gmail.com',
+        'Female',
+        '9920454526',
+        '03 June,2003',
+        'Economics',
+        'Sports',
+        'photo.jpg',
+        'Artistic driveway 4, sq.1',
+        'NCR Noida',
+    )
